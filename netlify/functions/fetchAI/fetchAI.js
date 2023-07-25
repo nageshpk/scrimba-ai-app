91a01d6feb63d1e5028e99c7340494334537ffaa
nagesh <npknagesh@gmail.com>
2023-07-25 21:01:52 +0530

    final refactor

--- netlify/functions/fetchAI/fetchAI.js
+++ netlify/functions/fetchAI/fetchAI.js
@@ -9,23 +9,23 @@ const openai = new OpenAIApi(configuration)
 
 const handler = async (event) => {
   try {
-    const response = await openai.createCompletion({
-        model: 'davinci:ft-personal-2023-07-21-10-27-32',
-        prompt: event.body,
-        presence_penalty: 0,
-        frequency_penalty: 0.3,
-        max_tokens: 100,
-        temperature: 0,
-        stop: ['\n', '->']
-    })
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ 
-        'reply': response.data
-       }),
-    }
+      const response = await openai.createCompletion({
+          model: 'davinci:ft-personal-2023-07-21-10-27-32',
+          prompt: event.body,
+          presence_penalty: 0.3,
+          frequency_penalty: 0,
+          max_tokens: 500,
+          temperature: 0,
+          stop: ['\n', '->']
+      })
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ 
+          'reply': response.data
+        }),
+      }
   } catch (error) {
-    return { statusCode: 500, body: error.toString() }
+      return { statusCode: 500, body: error.toString() }
   }
 }
 
