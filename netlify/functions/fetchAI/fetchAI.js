5ed33e9668ec87e05037db26dd6a637605f1c8a0
nagesh <npknagesh@gmail.com>
2023-07-25 20:14:07 +0530

    refactored fetchAI event handler

--- netlify/functions/fetchAI/fetchAI.js
+++ netlify/functions/fetchAI/fetchAI.js
@@ -1,13 +1,29 @@
-// Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
+import { Configuration, OpenAIApi } from 'openai'
+// import { process } from './env'
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+})
+
+const openai = new OpenAIApi(configuration)
+
 const handler = async (event) => {
   try {
+    const response = await openai.createCompletion({
+        model: 'davinci:ft-personal-2023-07-21-10-27-32',
+        prompt: event.body,
+        presence_penalty: 0,
+        frequency_penalty: 0.3,
+        max_tokens: 100,
+        temperature: 0,
+        stop: ['\n', '->']
+    })
     const subject = event.queryStringParameters.name || 'World'
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
+      body: JSON.stringify({ 
+        'reply': response.data
+       }),
     }
   } catch (error) {
     return { statusCode: 500, body: error.toString() }
